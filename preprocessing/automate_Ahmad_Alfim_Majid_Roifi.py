d8171195a7a6b984795ce8bdf2d17f822caa3e39
godmajid <alfinmajid19@gmail.com>
2025-12-23 18:59:35 +0700

    fisx path 3

--- preprocessing/automate_Ahmad_Alfim_Majid_Roifi.py
+++ preprocessing/automate_Ahmad_Alfim_Majid_Roifi.py
@@ -3,32 +3,25 @@ import os
 from sklearn.preprocessing import StandardScaler
 
 
-def preprocess_data(input_path: str, output_path: str):
-
-    # 1. Load Dataset
+def preprocess_data(input_path, output_path):
     df = pd.read_csv(input_path)
 
-    # 2. Handling Missing Values
     df['ph'] = df['ph'].fillna(df['ph'].mean())
     df['Sulfate'] = df['Sulfate'].fillna(df['Sulfate'].mean())
     df['Trihalomethanes'] = df['Trihalomethanes'].fillna(
         df['Trihalomethanes'].mean()
     )
 
-    # 3. Feature Scaling
     features_to_scale = df.drop('Potability', axis=1).columns
 
     scaler = StandardScaler()
     df[features_to_scale] = scaler.fit_transform(df[features_to_scale])
 
-    # 4. Outlier Handling (Clipping)
     for col in features_to_scale:
-        upper_limit = df[col].mean() + 3 * df[col].std()
-        lower_limit = df[col].mean() - 3 * df[col].std()
-        df[col] = df[col].clip(lower=lower_limit, upper=upper_limit)
+        upper = df[col].mean() + 3 * df[col].std()
+        lower = df[col].mean() - 3 * df[col].std()
+        df[col] = df[col].clip(lower, upper)
 
-
-    # 5. Save Preprocessed Dataset
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     df.to_csv(output_path, index=False)
 
@@ -36,7 +29,13 @@ def preprocess_data(input_path: str, output_path: str):
 
 
 if __name__ == "__main__":
-    preprocess_data(
-        input_path="godmajid\Eksperimen_SML_Ahmad-Alfim-Majid-Roifi\water_potability_raw.csv",
-        output_path="godmajid\Eksperimen_SML_Ahmad-Alfim-Majid-Roifi\preprocessing\water_potability_preprocessing"
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    input_csv = os.path.join(BASE_DIR, "water_potability_raw.csv")
+    output_csv = os.path.join(
+        BASE_DIR,
+        "preprocessing",
+        "water_potability_preprocessing.csv"
     )
+
+    preprocess_data(input_csv, output_csv)
